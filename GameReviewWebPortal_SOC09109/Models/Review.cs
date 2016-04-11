65f0a76e27e99d0495d1dfcaa803c6af9328268d
Mark Mclaughlin <mark_mclaughlin@live.co.uk>
2016-04-11 23:24:49 +0100

    Added some models, changed some web configuration.

---
+++ GameReviewWebPortal_SOC09109/Models/Review.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameReviewWebPortal_SOC09109.Models
+{
+    /// <summary>
+    /// Review Model
+    /// Used to retrieve reviews from the local database
+    /// </summary>
+    public class Review
+    {
+        /// <summary>
+        /// The Reviews ID
+        /// </summary>
+        public int ReviewID { get; set; }
+
+        /// <summary>
+        /// The reviewer
+        /// </summary>
+        public User Reviewer { get; set; }
+
+        /// <summary>
+        /// The title of the review
+        /// </summary>
+        public String Title { get; set; }
+
+        /// <summary>
+        /// The description of the review
+        /// </summary>
+        public String Description { get; set; }
+
+        /// <summary>
+        /// The ID of the comments left for this review
+        /// </summary>
+        public List<Comment> Comments { get; set; }    
+    }
+}
